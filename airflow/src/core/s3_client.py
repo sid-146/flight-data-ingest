0a285f470fbb4e5f535e41ee26865d2e1bbb09b5
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-09-27 15:41:31 +0530

    Updated s3 client file

--- airflow/src/core/s3_client.py
+++ airflow/src/core/s3_client.py
@@ -1,13 +1,54 @@
+import io
+import os
+import gzip
+import json
+from typing import ByteString
+
 import boto3
 
 
 class S3Client:
-    def __init__(self, region: str = "ap-south-1"):
-        self.s3_resource = boto3.resource("s3", region)
+    def __init__(
+        self,
+        bucket,
+        access_key: str,
+        secret_key: str,
+        region: str = "ap-south-1",
+    ):
+        self._bucket = bucket
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region,
+        )
 
-    def create_bucket(self, bucket_nam, region: str = "ap-south-1"):
-        self.s3_resource.create_bucket()
+    @property
+    def bucket(self):
+        return self._bucket
 
-    def list_buckets(self):
-        buckets = self.s3_resource.buckets.all()
+    @bucket.setter
+    def bucket(self, value):
+        self._bucket = value
+
+    def create_bucket(self, bucket_name, region):
+        raise NotImplementedError(
+            f"Function {self.create_bucket.__name__} not implemented. "
+        )
+        self.s3_client.create_bucket()
+
+    def list_buckets(self) -> None:
+        buckets = self.s3_client.buckets.all()
         print(*[bucket.name for bucket in buckets], sep="\n\t")
+
+    def put_compressed_object(self, results, bucket_name, put_path, ratio: int = 7):
+        s3_key = os.path.join(put_path, "data.json.gz")
+
+        buffer = io.BytesIO()
+        with gzip.GzipFile(fileobj=buffer, mode="wb", compresslevel=ratio) as f:
+            f.write(json.dumps(results, indent=2).encode("utf-8"))
+
+        buffer.seek(0)
+        self.s3_client.upload_fileobj(buffer, bucket_name, s3_key)
+
+        return f"s3://{bucket_name}/{s3_key}"
