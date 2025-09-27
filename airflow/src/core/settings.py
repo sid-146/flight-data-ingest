3575b4a866bbf146215226295ea6725b84a440fd
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-09-27 16:47:02 +0530

    updated settings

--- airflow/src/core/settings.py
+++ airflow/src/core/settings.py
@@ -1,8 +1,14 @@
+import os
+
 POSTGRES_DB: str = "flight_db"
 POSTGRES_PASSWORD: str = "airflow_password"
 POSTGRES_USER: str = "airflow_user"
 POSTGRES_HOST: str = "localhost"
 POSTGRES_PORT: int = 5432
-_schema: str = "flight_data"
+_schemas = ["flight_data", "monitoring"]
 
 postgres_connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+aws_secret_key = os.getenv("AWS_SECRET_KEY")
+access_key = os.getenv("AWS_ACCESS_KEY_ID")
+
