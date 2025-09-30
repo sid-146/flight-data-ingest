5709e4e34b9d0b576599d8c96bc6d08a5c67031f
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-09-30 13:59:24 +0530

    Update monitoring_models.py
    - Added ingestion_file_log.py

--- airflow/src/db/models/monitoring_models.py
+++ airflow/src/db/models/monitoring_models.py
@@ -11,19 +11,11 @@ from sqlalchemy import (
     DateTime,
     func,
     Sequence,
+    Text,
 )
 
-# from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship
 from src.db.config import BASE
-
-
-# class TestTable(BASE):
-#     __tablename__ = "testTable"
-#     __table_args__ = {"schema": "monitoring"}
-#     id: str = Column(String, nullable=False, primary_key=True)
-#     dag_name: str = Column(String, nullable=False)
-
 
 class ProcessRunLog(BASE):
     __tablename__ = "process_run_log"
@@ -38,3 +30,28 @@ class ProcessRunLog(BASE):
     dag_name: str = Column(String, nullable=False)
     starttime: datetime = Column(DateTime, nullable=False, default=func.now())
     status: str = Column(String)
+    task: str = Column(String, nullable=True)
+    error: str = Column(Text, nullable=True)
+    
+    files = relationship('IngestionFileLog', back_populate=)
+
+
+class IngestionFileLog(BASE):
+    __tablename__ = "ingestion_file_log"
+    __table_args__ = {"schema": "monitoring"}
+
+    id: int = Column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True,
+        server_default=Sequence("ingestion_file_log_id_seq").next_value(),
+    )
+    run_id:int = Column(
+        BigInteger,
+        ForeignKey("monitoring.process_run_log.id")
+    )
+    s3_key: str = Column(
+        String
+    ) 
+    
+    process_run = relationship("ProcessRunLog", back_populates='')
