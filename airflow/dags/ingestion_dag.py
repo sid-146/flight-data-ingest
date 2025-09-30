c7b01f20b21e5897fcf483675b923c1b2387d771
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-09-30 21:16:19 +0530

    Fixed task id issue.

--- airflow/dags/ingestion_dag.py
+++ airflow/dags/ingestion_dag.py
@@ -3,17 +3,21 @@ from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 
-from src.ingest.main import insert_log_entry,ingest_data
+from src.ingest.main import insert_log_entry, ingest_data
 
 
 with DAG(
-    dag_id="data_ingestion_data",
+    dag_id="data_ingestion_DAG",
     description="DAG to ingest data from api and upload to s3",
     start_date=datetime(2025, 9, 29),
     schedule="*/5 * * * *",
     catchup=False,
 ) as dag:
-    process_log_insert_task = PythonOperator(task_id="Process Log Insertion Task", python_callable=insert_log_entry)
-    ingest_data_task = PythonOperator(task_id = "Data Extraction Task", python_callable=ingest_data)
+    process_log_insert_task = PythonOperator(
+        task_id="process_log_insert_task", python_callable=insert_log_entry
+    )
+    ingest_data_task = PythonOperator(
+        task_id="data_extraction_task", python_callable=ingest_data
+    )
 
 process_log_insert_task >> ingest_data_task
