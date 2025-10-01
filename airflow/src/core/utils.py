71bb3c35740dd382f4373ada8f60148533f88e8a
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-10-01 17:16:09 +0530

    Update utils.py

--- airflow/src/core/utils.py
+++ airflow/src/core/utils.py
@@ -4,13 +4,15 @@ import os
 from datetime import datetime
 from typing import Callable, Tuple, Dict, List, Any
 from concurrent.futures import ThreadPoolExecutor, Future
+from logger import console
 
 
 def compress(results: list, store_path: str, ratio: int = 5):
     folder_name = f"flights_data_{datetime.now().strftime('%y_%m_%d_%H_%M_%S')}"
     path = os.path.join(store_path, folder_name)
+    os.makedirs(path, exist_ok=True)
     with gzip.open(
-        path,
+        os.path.join(path, "data.json.gz"),
         "wt",
         encoding="utf-8",
         compresslevel=ratio,
@@ -23,7 +25,7 @@ def generate_futures(
 ) -> Dict[Future, Tuple[Any, ...]]:
     # generic function to create futures, returns dictionary {futures:args}
     futures: Dict[Future, Tuple[Any, ...]] = {}
-    print(f"Creating futures for {func.__name__}")
+    console.info(f"Creating futures for {func.__name__}")
     with ThreadPoolExecutor(max_workers=max_workers) as pool:
         for arg in args:
             future = pool.submit(func, *arg)
