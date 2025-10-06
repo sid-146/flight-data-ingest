50c805a08a287fb70ef7b95dffb5f73752e73815
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-10-06 18:04:24 +0530

    Update main.py

--- airflow/src/airline_handler/main.py
+++ airflow/src/airline_handler/main.py
@@ -7,9 +7,16 @@ from src.db.models.monitoring_models import ProcessRunLog
 
 from src.core.logger import console
 from src.core.flight_api import FlightApiClient
+from src.core.utils import pull_from_xcom
 
 
-def insert_log_entry(**context):
+def pull_from_xcom(task_ids, key, **context):
+    # This can be moved to utils or core as it is generalized function.
+    value = context["ti"].xcom_pull(task_ids=task_ids, key=key)
+    return value
+
+
+def insert_log_entry_task(**context):
     console.info("Starting Insert Log Entry TASK.")
 
     monitoring_client = MonitoringClient()
@@ -24,18 +31,22 @@ def insert_log_entry(**context):
     context["ti"].xcom_push(key="inserted_id", value=inserted_id)
 
 
-def get_airlines(**context):
+def refresh_airline_task(**context):
     # Todo: Complete this function to create and store file accordingly.
+    # fetch id for which record was inserted.
+    # Updated process status with current task status.
+    # Get Flight Details using flight client
+    # Store and push to xcom the file path
+    # Update process Status
     try:
-        inserted_id = context["ti"].xcom_pull(
-            task_ids="process_log_insert_task", key="inserted_id"
-        )
+        _id = pull_from_xcom("process_log_insert_task", "inserted_id", **context)
 
+        # Creating Required objects
         monitoring_client = MonitoringClient()
+        f_api = FlightApiClient()
+
         results = (
-            monitoring_client.query(ProcessRunLog)
-            .where(ProcessRunLog.id == inserted_id)
-            .all()
+            monitoring_client.query(ProcessRunLog).where(ProcessRunLog.id == _id).all()
         )
 
         if not isinstance(results, list):
@@ -44,10 +55,19 @@ def get_airlines(**context):
         for result in results:
             console.info(result.to_dict())
 
+        console.info("Calling API to get airlines")
+        airlines = f_api.get_airlines()
+        if airlines:
+            console.info(f"Got {len(airlines)} airlines to process.")
+        else:
+            raise ValueError("API call completed but it returned empty list.")
+
         console.info("Updating status")
+        # Todo: this update function can be moved to client
+        # Issues: How to send multiple where condition (loop column: value and append to query (How to combination))
         results = (
             monitoring_client.query(ProcessRunLog)
-            .where(ProcessRunLog.id == inserted_id)
+            .where(ProcessRunLog.id == _id)
             .update(
                 {
                     "status": "Completed",
@@ -61,12 +81,15 @@ def get_airlines(**context):
 
     except Exception as e:
         console.error(f"Failed to get_airlines : {e.__class__.__name__} : {e}")
-        monitoring_client.query(ProcessRunLog).where(
-            ProcessRunLog.id == inserted_id
-        ).update(
+        monitoring_client.query(ProcessRunLog).where(ProcessRunLog.id == _id).update(
             {
                 "status": "Failed",
                 "errors": f"Failed to get_airlines : {e.__class__.__name__} : {e}",
                 "task": "airline_extraction_task",
             }
         ).all()
+
+
+def upload_s3_task(**context):
+    _id = pull_from_xcom("process_log_insert_task", "inserted_id", **context)
+    return _id
