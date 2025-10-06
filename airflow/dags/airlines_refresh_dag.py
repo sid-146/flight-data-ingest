27093ede1f90897a9a2f770ed8bee0e2dbf7e476
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-10-06 22:27:26 +0530

    fixed dag issue for task rename

--- airflow/dags/airlines_refresh_dag.py
+++ airflow/dags/airlines_refresh_dag.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 
-from src.airline_handler.main import insert_log_entry, get_airlines
+from src.airline_handler.main import (
+    insert_log_entry_task,
+    refresh_airline_task,
+    upload_s3_task,
+)
 
 
 with DAG(
@@ -14,10 +18,14 @@ with DAG(
     catchup=False,
 ) as dag:
     process_log_insert_task = PythonOperator(
-        task_id="process_log_insert_task", python_callable=insert_log_entry
+        task_id="process_log_insert_task", python_callable=insert_log_entry_task
     )
     airlines_extraction_task = PythonOperator(
-        task_id="airline_extraction_task", python_callable=get_airlines
+        task_id="refresh_airline_task", python_callable=refresh_airline_task
+    )
+    s3_upload_task = PythonOperator(
+        task_id="s3_upload_task", python_callable=upload_s3_task
     )
 
-process_log_insert_task >> airlines_extraction_task
+
+process_log_insert_task >> airlines_extraction_task >> s3_upload_task
