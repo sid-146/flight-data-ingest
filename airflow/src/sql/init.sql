9c77d2030d55a6bd0e5bfd0e15411d007ca6a10f
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-10-14 17:05:17 +0530

    Update init.sql

--- airflow/src/sql/init.sql
+++ airflow/src/sql/init.sql
@@ -133,3 +133,22 @@ ALTER SEQUENCE monitoring.ingestion_file_log_id_seq OWNED BY monitoring.ingestio
 ALTER TABLE monitoring.ingestion_file_log
 ADD COLUMN data_type varchar,
 ADD COLUMN is_processed varchar DEFAULT 'pending';
+
+-- Alter airlines tables
+ALTER TABLE flight_data.airline
+ADD COLUMN IF NOT EXISTS record_created_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN IF NOT EXISTS record_updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
+
+-- -- Added trigger function
+-- CREATE
+-- OR REPLACE FUNCTION flight_data.set_updated_at () RETURNS TRIGGER AS
+-- -- $$
+-- BEGIN NEW.updated_at = now ();
+-- RETURN NEW;
+-- END;
+-- -- $$ LANGUAGE plpgsql;
+
+
+-- Added Trigger
+-- CREATE TRIGGER set_timestamp BEFORE
+-- UPDATE ON flight_data.airlines FOR EACH ROW EXECUTE FUNCTION flight_data.set_updated_at ();
